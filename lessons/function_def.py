f212ddc77d0e673ad714bbf3595174b4461b6925
Victoria Rector <rrosevic@email.unc.edu>
2022-09-08 12:33:41 -0400

    commit

--- lessons/function_def.py
+++ lessons/function_def.py
@@ -3,9 +3,11 @@
 def my_max(a: int, b: int) -> int:
     """Returns the largest argument"""
     if a >= b:
-        return a
-    else:
-        return b  
+        return a 
+    
+    return b  
+
+
 
 x: int = 6
 y: int = 5 + 2

de9e53d285edd73a213d7664098c7d0e938b5700
Victoria Rector <rrosevic@email.unc.edu>
2022-09-20 19:49:35 -0400

    general backup

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -1,30 +1,31 @@
 """An exercise to practice list utility functions."""
 __author__: str = "730575822"
 
+
 def all(intlist: list[int], singleint: int) -> bool:
     """Evaluates whether all ints in the list are the given int."""
     i: int = 0
+    if len(intlist) == 0:
+        return False
     while i < len(intlist):
-        if intlist[i] == singleint:
-            i += 1
-        elif len(intlist) == 0:
+        if intlist[i] != singleint:
             return False
-        else:
-            return False
+        i += 1
     return True
-# still returns true if the list is empty, w/o elif. ask in OH
+
 
 def max(input: list[int]) -> int:
     """Returns largest value of a list of integers."""
     if len(input) == 0:
         raise ValueError("max() arg is an empty List")
-    i: int = 0
-    index: int = 0
+    i: int = 1
+    index: int = input[0]
     while i < len(input):
         if input[i] > index:
             index = input[i]
         i += 1 
     return index
+
 
 def is_equal(firstlist: int, secondlist: int) -> bool:
     """Based on deep equality, checks to see if two lists are exactly equal."""
@@ -32,4 +33,3 @@ def is_equal(firstlist: int, secondlist: int) -> bool:
         return True
     else:
         return False
-
